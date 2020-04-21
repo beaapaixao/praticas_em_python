c19061edf1b799487b38c2cfaed9ecb4f19a585b
beaapaixao <brendacefet@gmail.com>
2020-04-21 20:42:50 -0300

    Area da circunferencia - Fundamentos

---
+++ Fundamentos_Projetos/area_circunferencia_v1.py
@@ -0,0 +1,4 @@
+raio = 2
+pi = 3.1415
+area = pi * (raio ** 2)
+print(area)
---
+++ preview.py
@@ -0,0 +1,18 @@
+print(True)
+print(False)
+print(1.2 + 1)
+print('Aqui eu falo na minha língua')
+print("Tambem funciona")
+print("Você é " + 3 * 'muito ' + 'legal!')
+#print(3 + '3') --> Ambiguidade
+print([1,2,3]) # Lista
+print({'nome' : 'Pedro' ,'idade':22}) # Dicionário usa chave e valor
+print(None)
+
+
+
+
+
+
+lista = [1,2,3]
+dicionario = {'Nome':'Pedro', 'idade': 22}
