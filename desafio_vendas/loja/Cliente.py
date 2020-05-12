3af302ab8506e0648136005d5b5e19402dbb6134
beaapaixao <brendacefet@gmail.com>
2020-05-11 23:28:10 -0300

    Módulo Cliente

---
+++ desafio_vendas/loja/Cliente.py
@@ -0,0 +1,11 @@
+from .Pessoa import Pessoa
+
+
+class Cliente(Pessoa):
+
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
+        self.compras = []
+
+    def registrar_compra(self, compra):
+        self.compras.append(compra)
